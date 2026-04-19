09ae386683529dfe49877b8b43290deade3c3f76
obaidraza8130-cpu <obaidraza8130@gmail.com>
2026-04-19 19:10:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,44 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const fs = require('fs');
 const path = require('path');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // Home page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Form submit
+// Submit UID
 app.post('/submit', (req, res) => {
     const uid = req.body.uid;
-    console.log("Received UID:", uid);
-    res.send("UID received!");
+
+    fs.appendFileSync('data.txt', uid + "\n");
+
+    console.log("Saved UID:", uid);
+
+    res.send("UID saved successfully!");
 });
 
-// IMPORTANT for Render
+// View all UIDs
+app.get('/uids', (req, res) => {
+    if (!fs.existsSync('data.txt')) {
+        return res.send("No data yet");
+    }
+
+    const data = fs.readFileSync('data.txt', 'utf-8');
+
+    res.send(`
+        <h2>All UIDs</h2>
+        <pre>${data}</pre>
+        <br>
+        <a href="/">Back</a>
+    `);
+});
+
+// Server start
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server running...");
 });
